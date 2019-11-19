d367f9af9ba49f9145be832fc4f9930bf031ef58
Khady NDIAYE <diijandiaye6@gmail.com>
2019-11-19 16:37:40 +0100

    test maven

--- CabinetMedecin/CabinetMedecin-service/src/test/java/org/CabinetMedecin/service/TestMedecinService.java
+++ CabinetMedecin/CabinetMedecin-service/src/test/java/org/CabinetMedecin/service/TestMedecinService.java
@@ -16,8 +16,11 @@ public class TestMedecinService extends TestCase {
 
 		IMedecinDao dao = Mockito.mock(IMedecinDao.class);
 		MedecinService service = new MedecinService();
+
 		Mockito.when(dao.connect("FOBERLAY", "test")).thenReturn(true);
+		dao.connect("FOBERLAY", "test");
 		boolean valeur_retournée = service.authentification("FOBERLAY", "test");
+		Mockito.verify(dao, Mockito.times(1)).connect("FOBERLAY", "test");
 		Mockito.verify(dao).connect("FOBERLAY", "test");
 		TestCase.assertTrue(valeur_retournée);
 	}
